8e3834df71ef44b4749eb97eadc3a056fd099bf0
zarirhamza <zarir1999@gmail.com>
2019-09-16 20:12:51 -0400

    commit

---
+++ WildRydes/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_IuYeonpM0', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6gn842du2irs42mp3jkt7632th', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://0hjdozb3vf.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+    }
+};
